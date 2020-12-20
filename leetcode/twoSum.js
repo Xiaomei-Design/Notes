fbad7f417f6426bb31f1acb9323daef8fb715411
Xiaomei <xiaomei@Xiaomeis-MacBook-Air.local>
2020-12-21 00:00:27 +0100

    two sum

---
+++ leetcode/twoSum.js
@@ -0,0 +1,32 @@
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = 1; j < nums.length; j++) {
+            if(i === j) {
+                continue;
+            }
+            const sum = nums[i] + nums[j];
+            if (sum === target) {
+                return [i, j]
+            }
+        }
+    }
+};
+
+
+// use obj
+// but the runtime is the same, that's strange
+
+var twoSum = function(nums, target) {
+    let obj = {};
+    for(let i = 0; i< nums.length; i++) {
+        obj[target-nums[i]] = i
+    }
+    for( let j = 0; j < nums.length; j++) {
+        if(obj.hasOwnProperty(nums[j])) {
+            if(j === obj[nums[j]]) {
+                continue;
+            }
+            return [j, obj[nums[j]]]
+        }
+    }
+};
