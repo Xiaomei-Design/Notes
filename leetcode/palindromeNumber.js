976d1a8ee8b8c2e9456d18ee677d9dc0f9bdda7d
Xiaomei <xiaomei@Xiaomeis-Air.fritz.box>
2020-12-25 21:15:49 +0100

    palindrome number

---
+++ leetcode/palindromeNumber.js
@@ -0,0 +1,9 @@
+var isPalindrome = function(x) {
+    const xString = x.toString()
+    for(let i =0; i< xString.length/2; i++) {
+        if(xString[i] !== xString[xString.length -i-1]) {
+           return false;
+        }
+    }
+    return true;
+};
